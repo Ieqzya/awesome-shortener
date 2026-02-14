efb2fa5e7583ae06124498fe69d796f392d3dc0a
Евгений Захаров <evgenijzaharov@MacBook-Pro-von-Evgenij.local>
2026-02-14 17:30:58 +0300

    iter23

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -19,7 +19,18 @@ import (
 	"go.uber.org/zap"
 )
 
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
 func main() {
+	// Выводим информацию о сборке
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
+	fmt.Println()
 	// Инициализируем конфигурацию
 	cfg, err := config.NewConfig()
 	if err != nil {
